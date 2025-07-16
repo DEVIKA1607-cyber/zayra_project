a811d90a24000f8092104995f0c57adbee1fe087
Thanu-7 <thanushree1866@gmail.com>
2025-07-16 21:27:15 +0530

    commit model

--- prod_content/models.py
+++ prod_content/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from products.models import Product
+from accounts.models import User
 
-# Create your models here.
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image_url = models.ImageField(upload_to='products/')
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
--- prod_content/views.py
+++ prod_content/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def review_list(request):
+    return render(request, 'prod_content/review_list.html')
