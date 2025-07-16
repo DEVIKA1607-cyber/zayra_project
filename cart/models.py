39fdb879c6185eaa2ac921cb330447333d25dbd2
supriya050707 <supriyasenthilj577@gmail.com>
2025-07-16 21:31:58 +0530

    commit models

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from accounts.models import User
+from products.models import Product
 
-# Create your models here.
+class CartItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    saved_for_later = models.BooleanField(default=False)
+    added_at = models.DateTimeField(auto_now_add=True)
