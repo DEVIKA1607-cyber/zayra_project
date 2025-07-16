4b5ab09111ab7318c220427fe0e4ca8534fe9785
24004263 <n2500929@gmail.com>
2025-07-16 20:58:44 +0530

    add models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True)
+
+class Product(models.Model):
+    vendor = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={'role': 'vendor'})
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    brand = models.CharField(max_length=100)
+    tags = models.CharField(max_length=255)
+    stock = models.IntegerField(default=0)
+    rating_avg = models.FloatField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
