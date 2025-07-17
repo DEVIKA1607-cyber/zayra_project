3095b981c38a6c83f71968294adca9f1fe95c129
Devika <devika07durairaj@gmail.com>
2025-07-17 10:11:17 +0530

    success

--- analytics/models.py
+++ analytics/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from products.models import Product
 
-# Create your models here.
+class Analytics(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    daily_views = models.IntegerField(default=0)
+    cart_count = models.IntegerField(default=0)
+    orders_count = models.IntegerField(default=0)
+    search_terms = models.TextField(blank=True)
