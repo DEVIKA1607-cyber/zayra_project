dcd270906efae28781dae46de89a16d41bbdd020
Sangeetha24900442 <sangeetha1232007@gmail.com>
2025-07-17 22:25:27 +0530

    success

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def profile_detail(request):
-    return render(request, 'profiles/profile_detail.html')
+    return render(request, 'profiles/profile.html')
 
 def profile_edit(request):
     return render(request, 'profiles/profile_edit.html')
