4a73a4e5fa90733f0fa6842ef4afce0beaa3b247
Nethraa81 <nethraa132007@gmail.com>
2025-07-18 11:15:19 +0530

    success

--- payments/views.py
+++ payments/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
 def payment_list(request):
+    if request.method == 'POST':
+        amount = request.POST.get('amount')
+        upi_id = request.POST.get('upi_id')
+        return render(request, 'payments/payment_success.html', {
+            'amount': amount,
+            'upi_id': upi_id
+        })
     return render(request, 'payments/payment.html')
+def upi_payment(request):
+    return render(request, 'payments/upi.html')
