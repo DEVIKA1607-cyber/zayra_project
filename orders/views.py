c20d519c3d49a6ad22b5eb2af28b9b0c71e020b9
Nethraa81 <nethraa132007@gmail.com>
2025-07-18 12:06:45 +0530

    success

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def order_list(request):
-    return render(request, 'orders/order_list.html')
+    return render(request, 'orders/user_order.html')
 
 def order_detail(request, order_id):
     return render(request, 'orders/myorder.html')
