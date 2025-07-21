4e487a65199392ac4042a38949ff3af8266afea7
Sangeetha24900442 <sangeetha1232007@gmail.com>
2025-07-21 11:50:58 +0530

    success

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-
 urlpatterns = [
     path('profile/', views.profile_detail, name='profile_detail'),
     path('edit/', views.profile_edit, name='profile_edit'),
